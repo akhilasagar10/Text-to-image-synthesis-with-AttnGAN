48a464914cf6e0f0a9b3eebdf523392d5bb1e54e
beerus <siddharth.m98@gmail.com>
2022-03-18 22:47:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import demo
 app = MultiApp()
 st.set_page_config(page_title="GAN Project", initial_sidebar_state="auto")
 
-st.sidebar.title("Navigation")
-# Add all application here
 
 app.add_app("Bird GANs", demo.demo_gan)
 
